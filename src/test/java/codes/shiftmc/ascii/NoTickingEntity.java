df1f99bf1cd5046cfae287d670edee830c22b2dc
Sadness <rosalimavictor@gmail.com>
2024-12-05 16:50:50 -0300

    Screenshoot second monitor

--- src/test/java/codes/shiftmc/ascii/NoTickingEntity.java
+++ src/test/java/codes/shiftmc/ascii/NoTickingEntity.java
@@ -5,13 +5,10 @@ import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.metadata.display.TextDisplayMeta;
 import net.minestom.server.instance.Instance;
-import org.jetbrains.annotations.NotNull;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 public class NoTickingEntity extends Entity {
@@ -32,8 +29,22 @@ public class NoTickingEntity extends Entity {
 
         Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
         try {
-            BufferedImage capture = new Robot().createScreenCapture(screenRect);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            GraphicsDevice[] screens = ge.getScreenDevices();
+
+            int monitorIndex = 1;
+
+            if (monitorIndex >= screens.length) {
+                System.out.println("Monitor index out of bounds. Available monitors: " + screens.length);
+                return;
+            }
+
+            GraphicsDevice monitor = screens[monitorIndex];
+            Rectangle screenBounds = monitor.getDefaultConfiguration().getBounds();
+
+            BufferedImage capture = new Robot().createScreenCapture(screenBounds);
             capture = resizeImage(capture, screenRect.width / 15, screenRect.height / 15);
+
             meta.setText(ascii.getComponent(capture));
         } catch (AWTException e) {
             throw new RuntimeException(e);
@@ -44,16 +55,13 @@ public class NoTickingEntity extends Entity {
         BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, originalImage.getType());
         Graphics2D g2d = resizedImage.createGraphics();
 
-        // Enable anti-aliasing and quality scaling
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Draw the resized image
         g2d.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
         g2d.dispose();
 
         return resizedImage;
     }
-
 }
